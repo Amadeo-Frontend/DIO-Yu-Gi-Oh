15aae8c37eb1bd0c219ea72b3b0c8d8a0877c5d0
Amadeo Bon <amadeosph@outlook.com>
2024-10-20 13:13:58 -0300

    Implementando assinatura no js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,53 @@ const state = {
   },
 };
 
-function init() {}
+const playerSides = {
+    player1:"player-field-card",
+    computer:"computer-field-card"
+}
+
+const pathImages = "/src/assets/icons/";
+
+const cardData = [
+  {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    img: `${pathImages}"dragon.png"`,
+    winOf: [1],
+    loseOf: [2],
+  },
+  {
+    id: 1,
+    name: "Dark Magician",
+    type: "Rock",
+    img: `${pathImages}"magician.png"`,
+    winOf: [2],
+    loseOf: [0],
+  },
+  {
+    id: 2,
+    name: "Exodia",
+    type: "Scissors",
+    img: `${pathImages}"exodia.png"`,
+    winOf: [0],
+    loseOf: [1],
+  },
+];
+
+function drawCards (cardNumbers, fieldSide) {
+    for(let i = 0; i< cardNumbers; i++){
+        const randomIdCard = await getRandomCardId()
+        const cardImage = await createCardImage(randomIdCard, fieldSide)
+
+        document.getElementById(fieldSide).appendChild(cardImage)
+    }
+
+}
+
+function init() {
+  drawCards(5, playerSides.player1);
+  drawCards(5, playerSides.computer);
+}
 
 init();
